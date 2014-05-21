462b1fc85f100a55ef6c2be399daa89221900bc2
John Tracy Cunningham <johntc@gmail.com>
2014-05-21 23:29:42 +0200

    Completed Programming Assignment 2
    Tested with two 2 x 2 matrices, twice each, correct results.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function cachematrix.R by John Tracy Cunningham, 21 May 2014
 
-## Write a short comment describing this function
+## This function returns the inverse of a square matrix.  The input is
+## assumed to be a square matrix.  Use makeCacheMatrix first to create a
+## vector cache that will accept a matrix; then use cacheSolve to return the
+## inverse.  If the inverse of a matrix X has already been calculated, it has
+## been cached, and the cached inverse is not recalculated, but returned from the
+## cache.  If the inverse has not already been calculated, it is now, one time
+## only, and cached.  As calculating an inverse is resource-consuming, using a
+## cache saves time.
+
+## Create a vector cache with, for example, 
+## a <- makeCacheMatrix(x = matrix(1:4, 2, 2))
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        matinv <- list()
+        length(matinv) <- nrow(x) * ncol(x)
+        dim(matinv) = c(nrow(x), ncol(x))
+        set <- function(y = matrix()) {
+                x <<- matrix()
+                x <<- y
+                matinv <<- list()
+                length(matinv) <<- nrow(x) * ncol(x)
+                dim(matinv) <<- c(nrow(x), ncol(x))
+        }
+        get <- function() x
+        matrixinverse <- matrix()
+        setinv <- function(matrixinverse) matinv <<- matrixinverse
+        getinv <- function() matinv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Return the inverse of the matrix with, for example, cacheSolve(a)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix(), ...) {
+        matinv <- x$getinv()
+        if(!is.null(matinv[[1, 1]])) {
+                message("Getting cached data...")
+                return(matinv)
+        }
+        data <- x$get()
+        matinv <- solve(data, ...)
+        x$setinv(matinv)
+        matinv
 }
